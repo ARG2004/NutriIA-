af5b10895d4bf55926363864771ddb556904733f
NutrIA Developer <developer@nutria.com>
2026-08-15 10:57:14 -0600

    Deduplicate and optimize Android & Multiplatform library declarations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,7 +85,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.core.ktx)
     implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
